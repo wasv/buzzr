6bb3504d3db4cf9d79b21b147a852228bd412ce0
Billy Stevens <contact@wasv.me>
2019-12-07 07:23:03 -0500

    Removed unnecessary dependency.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "buzzr")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "dynamic-ffi"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/buzzr.scrbl" ())))
 (define pkg-desc "MQTT Bindings for Racket using paho")
 (define version "0.0.1")
